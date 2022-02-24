1ea4c4ca74442746ae2dae4f35b2152219649a15
neftec99 <neftalitorres@tec.mx>
2022-02-24 17:40:23 -0600

    Video: 333

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem{
+  ruta: string;
+  texto: string;
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
   ]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent  {
 
-  constructor() { }
+ menu : MenuItem [] = [
+   {ruta: '/graficas/barra', texto: 'Barras'  },
+   {ruta: '/graficas/barra-doble', texto: 'Barras Dobles'  },
+   {ruta: '/graficas/dona', texto: 'Dona'  },
+   {ruta: '/graficas/dona-http', texto: 'Dona http'  },
+ ]
 
-  ngOnInit(): void {
-  }
 
 }
