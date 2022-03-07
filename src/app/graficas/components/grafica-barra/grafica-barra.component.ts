b7abcb8ccc803bf7bdfbb82f37dfb62480c14ef6
neftec99 <neftalitorres@tec.mx>
2022-03-07 15:12:40 -0600

    Video: 337

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit,ViewChild  } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
-import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-grafica-barra',
@@ -13,38 +12,35 @@ export class GraficaBarraComponent implements OnInit {
 
 
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
-
-
-  public barChartOptions: ChartConfiguration['options'] = {
+ 
+  @Input() barChartOptions: ChartConfiguration['options'] = {
     responsive: true,
-  
   };
-  public barChartType: ChartType = 'bar';
-  public barChartPlugins = [
-    DataLabelsPlugin
-  ];
 
-  public barChartData: ChartData<'bar'> = {
-    labels: [ '2020', '2021', '2022', '2023', '2024', '2025', '2026' ],
+
+  @Input() barChartType: ChartType = 'bar';
+
+  @Input() barChartData: ChartData<'bar'> = {
+    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
     datasets: [
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series A' ,backgroundColor: '#ED5F76',hoverBackgroundColor:'red'},
-      { data: [ 8, 38, 70, 59, 66, 80, 100 ], label: 'Series B',backgroundColor: '#F763C4' ,hoverBackgroundColor:'red'}
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
     ]
   };
-
-   // events
-   public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
-    console.log(event, active);
-  }
-
-  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
-    console.log(event, active);
-  }
-
-
+ 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+    // events   pueden copiarlo o no
+    public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+      console.log(event, active);
+    }
+
+    public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+      console.log(event, active);
+    }
 
 }
--- src/app/graficas/pages/barras-doble/barras-doble.component.ts
+++ src/app/graficas/pages/barras-doble/barras-doble.component.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import {ChartDataset} from 'chart.js'
+import { ChartConfiguration, ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-barras-doble',
@@ -9,17 +9,36 @@ import {ChartDataset} from 'chart.js'
 })
 export class BarrasDobleComponent  {
 
-
-  proveedoresData: ChartDataset[] = [
-    { data: [ 100,200,300,400,500 ], label: 'Vendedor A' },
-    { data: [ 50,250,30, 450,200 ], label: 'Vendedor B' },
-  ];
+  // proveedoresData: ChartDataSets[] = [
+  //   { data: [ 100,200,300,400,500 ], label: 'Vendedor A' },
+  //   { data: [ 50,250,30, 450,200 ], label: 'Vendedor B' },
+  // ];
   
   proveedoresLabels: string[] = ['2021', '2022','2023','2024','2025'];
   
-  productoData: ChartDataset[] = [
-    { data: [ 200, 300,400,300, 100 ], label: 'Carros', backgroundColor: 'blue' },
-  ];
+  // productoData: ChartDataSets[] = [
+  //   { data: [ 200, 300,400,300, 100 ], label: 'Carros', backgroundColor: 'blue' },
+  // ];
+
+  productosOpciones: ChartConfiguration['options'] = {
+    responsive: true,
+    indexAxis: 'y', //Esto hace que sea horizontal el gráfico
+  }
  
+  proveedoresData: ChartData<'bar'> = {
+    labels: this.proveedoresLabels,
+    datasets: [
+      { data: [ 100,200,300,400,500 ], label: 'Vendedor A' },
+      { data: [ 50,250,30, 450,200 ], label: 'Vendedor B' },
+    ]
+  }
+ 
+
+  productosData: ChartData<'bar'> = {
+    labels: this.proveedoresLabels,
+    datasets: [
+      { data: [ 200, 300,400,300, 100 ], label: 'Carros', backgroundColor: 'blue' },
+    ]
+  }
 
 }
